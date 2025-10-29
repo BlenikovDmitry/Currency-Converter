3f1b08e0c3d43798467ae506e2c01ff9f6033de5
Блеников Дмитрий <158054275+BlenikovDmitry@users.noreply.github.com>
2025-10-30 00:06:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 #выводимые валюты в команде /values
 MAIN_VALUTES = ['EUR','USD','CNY', 'AUD', 'BGN', 'AED', 'JPY', 'INR']
 #Токен бота
-bot_token = '8370946420:AAHeHyw4TBJb7BrYtjOrsQZcsyF-5rQpw6o'
+bot_token = ''
+
